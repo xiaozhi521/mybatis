5348ff730a09c8cc4b656010432f02c69f80ab78
李洪智 <123456789>
2018-12-26 11:03:12 +0800

    update

--- src/main/java/com/org/dao/ContactInformationMapper.java
+++ src/main/java/com/org/dao/ContactInformationMapper.java
@@ -4,15 +4,18 @@ import com.org.bean.ContactInformation;
 
 /**
  *  使用session.getMapper 方式获取数据
- *  方式一、ContactInformationMapper contactInformationMapper = session.getMapper(ContactInformationMapper.class);
- *  ContactInformation contactInformation = contactInformationMapper.getContactInformationrById_1(1);
- *  需要注意在 *Mapper.xml 文件 namespace 的值是对应mapper类的dao接口 <mapper namespace="com.org.dao.ContactInformationMapper">
+ *      方式一、
+ *          ContactInformationMapper contactInformationMapper = session.getMapper(ContactInformationMapper.class);
+ *          ContactInformation contactInformation = contactInformationMapper.getContactInformationrById_1(1);
  *
- *  方式二、
- *  String statement = "com.org.mapper.ContactInformationMapper.getContactInformationrById";
- *  statement = "com.org.mapper.ContactInformationMapper.getContactInformationrById_1";
- *  ContactInformation contactInformation = session.selectOne(statement,1);
- *  需要注意在 *Mapper.xml 文件 namespace 的值是对应他自己的*Mapper.xml  <mapper namespace="com.org.Mapper.ContactInformationMapper">
+ *      需要注意在 *Mapper.xml 文件 namespace 的值是对应mapper类的dao接口 <mapper namespace="com.org.dao.ContactInformationMapper">
+ *
+ *      方式二、
+ *          String statement = "com.org.mapper.ContactInformationMapper.getContactInformationrById";
+ *          statement = "com.org.mapper.ContactInformationMapper.getContactInformationrById_1";
+ *          ContactInformation contactInformation = session.selectOne(statement,1);
+ *
+ *      需要注意在 *Mapper.xml 文件 namespace 的值是对应他自己的*Mapper.xml  <mapper namespace="com.org.Mapper.ContactInformationMapper">
  *
  */
 public interface ContactInformationMapper {
